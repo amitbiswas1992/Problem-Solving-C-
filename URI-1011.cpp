2f085994820f6547f74b15bf2d781ff0ec468901
Amit Biswas <biswas1992@hotmail.com>
2019-07-13 02:46:26 -0400

    URI-1011

---
+++ URI-1011.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+
+    float r;
+       cin >> r;
+
+       double  v;
+       v = ((4.0/3) * 3.14159 * (r * r * r));
+
+
+       printf("VOLUME = %.3lf\n",v);
+       return 0;
+
+}
