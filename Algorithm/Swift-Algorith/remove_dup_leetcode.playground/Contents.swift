b8f90cb1c68edbd5714717c4f36ac90cf9c889cc
Amit Biswas <biswas1992@hotmail.com>
2019-11-01 04:10:29 -0400

    Goolge Interview plan and contents

--- Algorithm/Swift-Algorith/remove_dup_leetcode.playground/Contents.swift
+++ Algorithm/Swift-Algorith/remove_dup_leetcode.playground/Contents.swift
@@ -2,16 +2,47 @@ import UIKit
 import Foundation
 
 
-    func removeDuplicates( nums: inout [Int]) -> Int {
-      
-        let set = Set<Int>(nums)
-        nums = Array(set)
-        nums.sort()
-        return nums.count
-    
 
-}
+    func mergeSort(array: [Int])-> [Int] {
 
-var arr = [0, 1, 2, 1, 2]
+        guard array.count > 1 else {
+            return array
+        }
 
-print(removeDuplicates(nums: &arr))
+        let leftarray: [Int] = Array(array[0..<array.count/2])
+        let rightArray: [Int] = Array(array[array.count/2..<array.count])
+
+
+        return merge(left: mergeSort(array: leftarray), right: mergeSort(array: rightArray))
+    }
+
+    func merge(left: [Int], right: [Int])-> [Int]{
+
+        var mergeArray: [Int] = []
+        var left = left
+        var right = right
+
+        while left.count > 0 && right.count > 0 {
+
+            if left.first! < right.first! {
+                mergeArray.append(left.removeFirst())
+            } else {
+
+                mergeArray.append(right.removeFirst())
+            }
+
+        }
+
+     return   mergeArray + left + right
+    }
+
+    var numbers : [Int] = []
+
+    for _ in 0..<10 {
+        let n = Int(arc4random_uniform(UInt32(1000)))
+        numbers.append(n)
+    }
+
+    print(numbers)
+
+    print(mergeSort(array: numbers))
