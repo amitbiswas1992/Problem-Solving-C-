ba82237fa38078f20e3400eca6e0ffcab2098b41
Amit Biswas <biswas1992@hotmail.com>
2019-12-11 01:21:15 -0500

    Reversed String Prac

--- Algorithm/Swift-Algorith/Reverse_String.swift
+++ Algorithm/Swift-Algorith/Reverse_String.swift
@@ -1,19 +1,52 @@
+//import Foundation 
+//
+//func reverseString(_ s: inout [Character]) -> [Character]{
+//	var j = s.count - 1
+//	for i in 0..<s.count{	
+//		let temp = s[i]
+//		s[i] = s[j]
+//		s[j] = temp
+//		j = j - 1
+//		if i >= j{
+//			break
+//		}
+//	}
+//	return s
+//}
+//
+//var a : [Character] = ["1", "2", "3", "4", "5", "6"]
+//
+//print (reverseString(&a))
+
 import Foundation 
 
-func reverseString(_ s: inout [Character]) -> [Character]{
-	var j = s.count - 1
-	for i in 0..<s.count{	
+func reverseS(_ s: inout [Character]){
+	var j = s.count - 1 
+	for i in 0..<s.count {
 		let temp = s[i]
 		s[i] = s[j]
 		s[j] = temp
+		
 		j = j - 1
 		if i >= j{
 			break
 		}
 	}
-	return s
+	
 }
 
-var a : [Character] = ["1", "2", "3", "4", "5", "6"]
+var a : [Character] = ["2", "3", "4", "8"]
+reverseS(&a)
+print(a)
 
-print (reverseString(&a))
+
+
+
+
+
+
+
+
+
+
+
