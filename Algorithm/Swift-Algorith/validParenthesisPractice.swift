8bb4865a9c6bbd65b1006afd3ca1f7b7120d39c9
Amit Biswas <biswas1992@hotmail.com>
2019-12-19 01:21:20 -0500

    Valid Parenthesys

---
+++ Algorithm/Swift-Algorith/validParenthesisPractice.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+func isValid(_ s: String) -> Bool {
+	struct Stack{
+		var characterArray: [Character] = []
+		
+		func size() -> Int {
+			return characterArray.count
+		}
+		
+		mutating func push(_ val: Character){
+			characterArray.append(val)
+		}
+		
+		mutating func pop(){
+			if characterArray.count > 0{
+				characterArray.removeLast()
+			}
+		}
+		
+		mutating func top() -> Character{
+			if characterArray.count > 0{
+				return characterArray.last!
+			}
+			return "*"
+		}
+	}
+	
+}
