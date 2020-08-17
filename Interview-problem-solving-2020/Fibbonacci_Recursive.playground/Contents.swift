dc38bf83b599b9ae7b5b5228cb9c2ed46ab7cb20
Amit Biswas <biswas1992@hotmail.com>
2020-08-17 11:36:49 -0400

    Fibonacci recursive

---
+++ Interview-problem-solving-2020/Fibbonacci_Recursive.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+//func fibbo(numOfStep: Int , first: Int, second: Int) -> [Int] {
+//
+//    if numOfStep == 0 {
+//        return []
+//    }
+//
+//    return [first + second] + fibbo(numOfStep: numOfStep - 1 , first: second, second: first + second)
+//
+//}
+//
+//fibbo(numOfStep: 9, first: 0, second: 1)
+
+
+func fibbo(numOfSteps: Int, first: Int, second: Int) -> [Int] {
+  
+    if numOfSteps == 0 {
+        return []
+    }
+    
+    return [first + second] + fibbo(numOfSteps: numOfSteps - 1 , first: second, second: first + second)
+    
+}
+
+fibbo(numOfSteps: 20, first: 0, second: 1)
