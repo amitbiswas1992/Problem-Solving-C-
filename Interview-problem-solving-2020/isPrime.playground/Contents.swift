1875f8a7776253498f955252e3dc6eae210b81a8
Amit Biswas <biswas1992@hotmail.com>
2020-08-27 03:50:08 -0400

    Prime in Swift

---
+++ Interview-problem-solving-2020/isPrime.playground/Contents.swift
@@ -0,0 +1,93 @@
+import UIKit
+
+//func isPrime (num: Int) -> Bool {
+//
+//    if num < 2 {
+//        return false
+//    }
+//
+//    for i in 2..<num {
+//        if num % i == 0 {
+//            return false
+//        }
+//    }
+//
+//    return true
+//}
+//
+//isPrime(num: 6392376210712736193)
+
+// Sheive
+//func countPrime(_ n : Int ) -> Int {
+//
+//    guard n > 2 else {
+//        return 0
+//    }
+//
+//    var prime = Array(repeating: true, count: n)
+//
+//    // number 0 and number 1 are not prime
+//
+//    prime[0] = false
+//    prime[1] = false
+//    var count = 0
+//
+//    for i in 2..<n {
+//        guard prime[i] else {
+//           continue
+//        }
+//        count += 1
+//
+//        // skip repeaing number
+//
+//        var index = i * i
+//        while index < n {
+//            prime[index] = false
+//            index += i
+//        }
+//
+//    }
+//
+//   return count
+//}
+//
+//countPrime(20)
+
+
+func countPrime(_ n : Int ) -> Int {
+ 
+    guard n > 2 else {
+        return 0
+    }
+    
+    var isPrime = Array(repeating: true , count : n)
+    
+     isPrime[0] = false
+     isPrime[1] = false
+     var count = 0
+    
+    for i in 2..<n {
+        
+        guard isPrime[i] else {
+            continue
+        }
+        count += 1
+        var index = i * i
+        
+        while index < n {
+            isPrime[index] = false
+            index += i
+        }
+        
+        
+    }
+    
+    for i in 2..<n {
+        if isPrime[i] {
+            print(i)
+        }
+    }
+    
+    return count
+}
+countPrime(10)
